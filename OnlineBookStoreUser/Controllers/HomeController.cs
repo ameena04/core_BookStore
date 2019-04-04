4a517db2967c63579b1579a418ee1605c26e0e8b
Ameena Bano <Ameena.Bano@NIIT-Tech.com>
2019-04-04 12:11:44 +0530

    Add project files.

---
+++ OnlineBookStoreUser/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineBookStoreUser.Helper;
+using OnlineBookStoreUser.Models;
+
+namespace OnlineBookStoreUser.Controllers
+{
+    public class HomeController : Controller
+    {
+        Book_Store_DbContext context = new Book_Store_DbContext();
+        public IActionResult Index()
+        {
+          
+            var books = context.Books.ToList();
+            int i = 0;
+            int j = 0;
+            var cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+            if (cart != null)
+            {
+                foreach (var item in cart)
+                {
+                    i++;
+                }
+                if (i != 0)
+                {
+                    foreach (var item in cart)
+                    {
+                        j++;
+                    }
+                }
+                HttpContext.Session.SetString("CartItem", i.ToString());
+            }
+            return View(books);
+
+        }
+        
+    }
+}
